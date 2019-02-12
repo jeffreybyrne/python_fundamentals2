ddf7a4178a9ffc6274f110ebd0c978c107ecc81e
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-12 13:43:22 -0500

    finished the fourth part

--- exercises.py
+++ exercises.py
@@ -40,3 +40,17 @@ def is_even(num):
 print('Is 0 even? {}'.format(is_even(0)))
 print('Is 13 even? {}'.format(is_even(13)))
 print('Is 2 even? {}'.format(is_even(2)))
+
+#Define a function that accepts a string as an argument and returns False if the word is less than 8 characters long
+#(or True otherwise).
+
+def longer_than_eight(num):
+    if len(num) < 8: #If the length is less than 8, return False
+        return False
+    else: #Otherwise, return True
+        return True
+
+print('Is the string \'Hello\' longer than eight characters? {}'.format(longer_than_eight('Hello')))
+print('Is the string \'Is it me you\'re looking for?\' longer than eight characters? {}'.format(longer_than_eight('Is it me you\'re looking for?')))
+print('Is the string \'I can see it in your eyes\' longer than eight characters? {}'.format(longer_than_eight('I can see it in your eyes')))
+print('Is the string \'I can see it in your smile\' longer than eight characters? {}'.format(longer_than_eight('I can see it in your smile')))
