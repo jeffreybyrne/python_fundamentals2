01de670ada20e2fed8f7fd359ca8160a0db807f4
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-12 13:24:38 -0500

    finished the first exercise

---
+++ exercises.py
@@ -0,0 +1,9 @@
+# Define a function called double that accepts an argument called my_number and returns that number multiplied by 2.
+# Try calling it multiple times and pass in different numbers each time.
+
+def double(my_number):
+    return 2*my_number
+
+print(double(5))
+print(double(13))
+print(double(-3.4))
