cedca8eada470d5e39b230e7df102fcabf1f6f6f
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-12 13:29:17 -0500

    finished the second exercise

--- exercises.py
+++ exercises.py
@@ -7,3 +7,19 @@ def double(my_number):
 print(double(5))
 print(double(13))
 print(double(-3.4))
+
+# Define a function called negative that accepts a number as an argument and returns a boolean (true/false) indicating
+#whether that number is negative or not.
+# Try calling it multiple times, passing in different numbers each time.
+
+def negative(num):
+    if (num == abs(num)): #If the number is equal to it's absolute value, it's positive
+        return False
+    elif (num*-1 == abs(num)): #If the number multiplied by negative one equals it's absolute value, it's negative
+        return True
+    else: #Otherwise it's either not a number, or it's zero which for simplicity we'll say isn't negative
+        return False
+
+print(negative(0))
+print(negative(100))
+print(negative(-100))
