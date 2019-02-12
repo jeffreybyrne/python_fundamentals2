c2223cc79fbac590fa2132c9f38f6e9b0fc42dd0
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-12 13:36:44 -0500

    finished the third part, cleaned up the printouts of the earlier exercises to make it easier to understand

--- exercises.py
+++ exercises.py
@@ -4,9 +4,9 @@
 def double(my_number):
     return 2*my_number
 
-print(double(5))
-print(double(13))
-print(double(-3.4))
+print('5 doubled is {}'.format(double(5)))
+print('13 doubled is {}'.format(double(13)))
+print('-3.4 doubled is {}'.format(double(-3.4)))
 
 # Define a function called negative that accepts a number as an argument and returns a boolean (true/false) indicating
 #whether that number is negative or not.
@@ -20,6 +20,23 @@ def negative(num):
     else: #Otherwise it's either not a number, or it's zero which for simplicity we'll say isn't negative
         return False
 
-print(negative(0))
-print(negative(100))
-print(negative(-100))
+print('Is 0 negative? {}'.format(negative(0)))
+print('Is 100 negative? {}'.format(negative(100)))
+print('Is -100 negative? {}'.format(negative(-100)))
+
+# Define a function called is_even that accepts a number as an argument and returns a boolean (true/false) indicating whether
+# that number is even or not (HINT: use the % operator).
+#
+# Try calling it with different numbers.
+
+def is_even(num):
+    if (num%2 == 0): #If the number mod 2 is 0, it's even so return True
+        return True
+    elif (num%2 == 1): #If the number mod 2 is 1, it's odd so return False
+        return False
+    else: #Otherwise, you've got something that most assuredly isn't an even number, so return False
+        return False
+
+print('Is 0 even? {}'.format(is_even(0)))
+print('Is 13 even? {}'.format(is_even(13)))
+print('Is 2 even? {}'.format(is_even(2)))
