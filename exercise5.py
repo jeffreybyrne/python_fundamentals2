874b3a7ca463524286a9d8f876459608413d46ee
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-12 13:53:15 -0500

    finished exercise 5

--- exercise5.py
+++ exercise5.py
@@ -11,3 +11,12 @@
 # Output the result in a full sentence using string interpolation.
 #
 # Don't forget to commit your progress as you go along. Once you're done, commit one last time and push it to github.
+
+print('Enter a temperature in Fahrenheit') #Prompt the user for input
+user_temp = int(input()) #Convert it to an integer and store it
+
+def f_to_c(temp): #Function takes a number
+    celsius = (temp - 32) * 5 / 9 #Does the math to convert it to celsius
+    return celsius #Return the celsius value
+
+print('{}F is {}C'.format(user_temp,f_to_c(user_temp))) #Show the user the temperature they entered and the converted temp as well
